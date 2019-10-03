c82e5a66b68baafb899f8165c4a06e0614cf32e7
m3lodicaman <33330721+m3lodicaman@users.noreply.github.com>
2019-10-03 07:40:17 -0400

    Created base game
    -Added player and grapple sprites
    -Added ground tiles
    -Added player movement
    -Partially integrated grapple mechanics

---
+++ Grapple Game/Assets/Scripts/GrappleController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappleController : MonoBehaviour
+{
+    public static bool shoot;
+
+    [SerializeField] private float offset;
+    [SerializeField] private float grappleSpeed;
+
+    private Rigidbody2D rb;
+    private bool grappleOnBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        grappleOnBody = true;
+        shoot = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(Input.mousePosition);
+        ShootGrapple();
+        PointToCursor();
+    }
+
+    void ShootGrapple()
+    {
+        if(shoot)
+        {
+            grappleOnBody = false;
+        }
+        shoot = false;
+    }
+
+    void PointToCursor()
+    {
+        if (grappleOnBody)
+        {
+            var direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle + offset, Vector3.forward);
+        }
+    }
+}
---
+++ Grapple Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float deceleration;
+    [SerializeField] private float jumpForce;
+    [Range(0.1f,0.4f)] [SerializeField] private float endMovementParam;
+    [SerializeField] private float crouchSpeedModifier;
+    [SerializeField] private Collider2D crouchDisableCollider;
+
+    private Animator an;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private float direction;
+    private bool jump;
+    private bool crouch;
+    private bool crouching;
+    private bool mustCrouch;
+    private bool grounded;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        grounded = true;
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        grounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        mustCrouch = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        mustCrouch = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        mustCrouch = false;
+    }
+
+    private void Start()
+    {
+        an = gameObject.GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        direction = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            jump = true;
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetKeyUp("s"))
+        {
+            crouch = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GrappleController.shoot = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(direction > 0f)
+        {
+            sr.flipX = false;
+
+            if(rb.velocity.x < maxSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x + acceleration, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+            }
+        }
+        if (direction < 0f)
+        {
+            sr.flipX = true;
+
+            if (rb.velocity.x > -maxSpeed)
+            {
+                rb.velocity = new Vector2(rb.velocity.x - acceleration, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+            }
+        }
+        if(direction == 0f)
+        {
+            if(rb.velocity.x > endMovementParam)
+            {
+                rb.velocity = new Vector2(rb.velocity.x - deceleration, rb.velocity.y);
+            }
+            if (rb.velocity.x < -endMovementParam)
+            {
+                rb.velocity = new Vector2(rb.velocity.x + deceleration, rb.velocity.y);
+            }
+            if (rb.velocity.x > -endMovementParam || rb.velocity.x < endMovementParam)
+            {
+                rb.velocity = new Vector2(0f, rb.velocity.y);
+            }
+        }
+
+        if (jump)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jump = false;
+        }
+
+        if (crouch && !crouching)
+        {
+            maxSpeed *= crouchSpeedModifier;
+            crouchDisableCollider.enabled = false;
+            crouching = true;
+        }
+        if(!crouch && !mustCrouch && crouching)
+        {
+            maxSpeed /= crouchSpeedModifier;
+            crouchDisableCollider.enabled = true;
+            crouching = false;
+        }
+    }
+}
